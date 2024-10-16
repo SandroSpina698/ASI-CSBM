b1943847f4057a1fd728c8b6e9911c4eb84535c5
Ceïf <cecece775@gmail.com>
2024-10-16 17:31:31 +0200

    add of basic of projects

---
+++ back/generate_property/src/main/java/com/csbm/generate_property/GeneratePropertyApplication.java
@@ -0,0 +1,13 @@
+package com.csbm.generate_property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratePropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratePropertyApplication.class, args);
+	}
+
+}
---
+++ back/generate_property/src/test/java/com/csbm/generate_property/GeneratePropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csbm.generate_property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratePropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
