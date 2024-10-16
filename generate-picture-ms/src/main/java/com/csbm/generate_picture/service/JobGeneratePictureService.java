9f6f221da2671c87a9a98426af04a92a0d4c0501
Maxime Brossard <maxime.brossard5@gmail.com>
2024-10-16 18:40:19 +0200

    fix: fixing error from mqtt

--- generate-picture-ms/src/main/java/com/csbm/generate_picture/service/JobGeneratePictureService.java
+++ generate-picture-ms/src/main/java/com/csbm/generate_picture/service/JobGeneratePictureService.java
@@ -1,12 +1,12 @@
 package com.csbm.generate_picture.service;
 
-import com.csbm.generate_picture.model.DTO.JobResultDTO;
+import com.csbm.generate_picture.model.DTO.ImageRequestDTO;
 import org.springframework.jms.annotation.JmsListener;
 
 public class JobGeneratePictureService {
 
-    @JmsListener(destination = "genPic")
-    public void processPictureGeneration(JobResultDTO jobResultDTO) {
-        System.out.println();
+    @JmsListener(destination = "gen-pic",containerFactory = "jmsListenerContainerFactory")
+    public void receivedMessage(String imageRequestDTO) {
+        System.out.println("Message reçu depuis le topic genPic : " + imageRequestDTO);
     }
 }
